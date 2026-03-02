71ad834d02f27e867c036b0a19f4029c8a7e8d62
igorermolenko <44259330+igorermolenko@users.noreply.github.com>
2026-03-02 21:58:36 +0200

    LeetCode 150

--- src/main/java/com/leetcode/BinaryTreeRightSideView.java
+++ src/main/java/com/leetcode/BinaryTreeRightSideView.java
@@ -40,6 +40,26 @@ public class BinaryTreeRightSideView {
         int level;
     }
 
+    public List<Integer> rightSideView2(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode node = null;
+            for (int i = 0; i < levelSize; i++) {
+                node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            result.add(node.val);
+        }
+
+        return result;
+    }
 
     public List<Integer> rightSideView(TreeNode root) {
         if (root == null) return Collections.emptyList();
