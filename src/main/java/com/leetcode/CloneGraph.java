6e06d4f2c5b28e2a0796f3bea669b632bed00cd1
igorermolenko <44259330+igorermolenko@users.noreply.github.com>
2026-03-10 09:24:16 +0200

    LeetCode 150

--- src/main/java/com/leetcode/CloneGraph.java
+++ src/main/java/com/leetcode/CloneGraph.java
@@ -2,19 +2,21 @@ package com.leetcode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class CloneGraph {
 
     public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
         return cloneNode(node, new HashMap<>());
     }
 
     private Node cloneNode(Node node, Map<Integer, Node> cloned) {
-        if (node == null) {
-            return null;
-        }
         Node clone = new Node(node.val);
         cloned.put(node.val, clone);
         node.neighbors.forEach(neighbor ->
@@ -23,6 +25,32 @@ public class CloneGraph {
                                 cloned.get(neighbor.val) : cloneNode(neighbor, cloned)));
         return clone;
     }
+
+    public Node cloneGraphBFS(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Map<Integer, Node> cloned = new HashMap<>();
+        cloned.put(node.val, new Node(node.val));
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            Node clone = cloned.get(current.val);
+            for (Node neighbor : current.neighbors) {
+                if (!cloned.containsKey(neighbor.val)) {
+                    cloned.put(neighbor.val, new Node(neighbor.val));
+                    queue.offer(neighbor);
+                }
+                clone.neighbors.add(cloned.get(neighbor.val));
+            }
+        }
+        return cloned.get(node.val);
+    }
+
+
 
 
     public static class Node {
@@ -52,13 +80,18 @@ public class CloneGraph {
         node4.neighbors.add(node3);
 
         Object cloned1 = task.cloneGraph(node1);
+        Object cloned1BFS = task.cloneGraphBFS(node1);
         System.out.println(cloned1);
+        System.out.println(cloned1BFS);
 
         Node node5 = new Node(1);
         Object cloned2 = task.cloneGraph(node5);
+        Object cloned2BFS = task.cloneGraphBFS(node5);
         System.out.println(cloned2);
+        System.out.println(cloned2BFS);
 
         System.out.println(task.cloneGraph(null));
+        System.out.println(task.cloneGraphBFS(null));
 
     }
 }
