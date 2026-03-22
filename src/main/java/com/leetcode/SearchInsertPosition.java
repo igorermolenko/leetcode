1e8da7aabfdfa4bd0604983e3a16f43ff02f3b02
igorermolenko <44259330+igorermolenko@users.noreply.github.com>
2026-03-22 21:38:01 +0200

    LeetCode 150

---
+++ src/main/java/com/leetcode/SearchInsertPosition.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+
+    public static void main(String[] args) {
+        SearchInsertPosition task = new SearchInsertPosition();
+        System.out.println(task.searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(task.searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(task.searchInsert(new int[]{1, 3, 5, 6}, 7));
+    }
+}
