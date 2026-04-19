d78d7677455ab0b56e451b0d1eeaacea1908857b
igorermolenko <44259330+igorermolenko@users.noreply.github.com>
2026-04-19 21:50:35 +0300

    LeetCode 150

--- src/main/java/com/leetcode/MaximumSubarray.java
+++ src/main/java/com/leetcode/MaximumSubarray.java
@@ -2,6 +2,8 @@ package com.leetcode;
 
 public class MaximumSubarray {
 
+    // =========================================
+    // Kadane's Algorithm       --- O(n)
     public int maxSubArray(int[] nums) {
         int result = Integer.MIN_VALUE;
         int sum = 0;
@@ -14,6 +16,40 @@ public class MaximumSubarray {
         }
 
         return  result;
+    }
+
+
+    // =========================================
+    // Divide and Conquer        --- O(n * log(n))
+    public int maxSubArray2(int[] nums) {
+        return maxSubArray(nums, 0, nums.length - 1);
+    }
+
+    private int maxSubArray(int[] nums, int low, int high) {
+        if (low == high) {
+            return nums[low];
+        }
+        int middle = low + (high - low) / 2;
+        int left = maxSubArray(nums, low, middle);
+        int right = maxSubArray(nums, middle + 1, high);
+        int crossing = crossingMax(nums, low, high, middle);
+        return Math.max(crossing, Math.max(left, right));
+    }
+
+    private int crossingMax(int[] nums, int low, int high, int middle) {
+        int maxLeft = Integer.MIN_VALUE;
+        int maxRight = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = middle; i >= low; i--) {
+            sum += nums[i];
+            maxLeft = Math.max(maxLeft, sum);
+        }
+        sum = 0;
+        for (int i = middle + 1; i <= high; i++) {
+            sum += nums[i];
+            maxRight = Math.max(maxRight, sum);
+        }
+        return maxLeft + maxRight;
     }
 
 
