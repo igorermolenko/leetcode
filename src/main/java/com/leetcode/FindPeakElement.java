287880a21f8feebdcbccb9793afe989e7fe846ad
igorermolenko <44259330+igorermolenko@users.noreply.github.com>
2026-05-01 10:41:51 +0300

    LeetCode 150

--- src/main/java/com/leetcode/FindPeakElement.java
+++ src/main/java/com/leetcode/FindPeakElement.java
@@ -1,37 +1,38 @@
 package com.leetcode;
 
-public class PeakElement {
+public class FindPeakElement {
 
     public int findPeakElement(int[] nums) {
-        int leftBound = 0;
-        int rightBound = nums.length;
+        int leftIdx = 0;
+        int rightIdx = nums.length - 1;
 
-        while (leftBound < rightBound) {
-            int midIdx = leftBound + (rightBound - leftBound) / 2;
+        while (leftIdx <= rightIdx) {
+            int midIdx = leftIdx + (rightIdx - leftIdx) / 2;
 
             int mid = nums[midIdx];
-            long left = midIdx > 0 ? nums[midIdx - 1] : Long.MIN_VALUE;
-            long right = midIdx < nums.length - 1 ? nums[midIdx + 1] : Long.MIN_VALUE;
+            int left = midIdx > 0 ? nums[midIdx - 1] : Integer.MIN_VALUE;
+            int right = midIdx < nums.length - 1 ? nums[midIdx + 1] : Integer.MIN_VALUE;
 
-            if (mid > left && mid > right) {
+            if (mid >= left && mid >= right) {
                 return midIdx;
             }
-            if (mid < left) {
-                rightBound = midIdx;
-            } else {
-                leftBound = midIdx;
-            }
 
+            if (mid < left) {
+                rightIdx = midIdx - 1;
+            } else {
+                leftIdx = midIdx + 1;
+            }
         }
+
         return -1;
     }
 
 
-
     public static void main(String[] args) {
-        PeakElement task = new PeakElement();
+        FindPeakElement task = new FindPeakElement();
         System.out.println(task.findPeakElement(new int[]{1}));
         System.out.println(task.findPeakElement(new int[]{-2147483648}));
+        System.out.println(task.findPeakElement(new int[]{1, 2, 2, 2, 1}));
         System.out.println(task.findPeakElement(new int[]{1, 2, 3, 1}));
         System.out.println(task.findPeakElement(new int[]{1, 2, 3, 4}));
         System.out.println(task.findPeakElement(new int[]{5, 4, 3, 2}));
