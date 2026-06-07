99171e1125427892402e2b2a546fc4727c27a7e2
igorermolenko <44259330+igorermolenko@users.noreply.github.com>
2026-06-07 18:24:26 +0300

    LeetCode 150

--- src/main/java/com/leetcode/ClimbingStairs.java
+++ src/main/java/com/leetcode/ClimbingStairs.java
@@ -3,17 +3,27 @@ package com.leetcode;
 public class ClimbingStairs {
 
     public int climbStairs(int n) {
-        if (n < 3) return n;
+        if (n <= 1) return n;
 
-        int prePreviousStair = 1;
-        int previousStair = 2;
-        for (int i = 3; i <= n; i++) {
-            int currentStair = prePreviousStair + previousStair;
-            prePreviousStair = previousStair;
-            previousStair = currentStair;
+        int prevPrev = 1, prev = 2;
+        for (int i = 0; i < n - 2; i++) {
+            int curr = prevPrev + prev;
+            prevPrev = prev;
+            prev = curr;
         }
+        return prev;
+    }
 
-        return previousStair;
+    public int climbStairs2(int n) {
+        if (n <= 1) return n;
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
     }
 
 
